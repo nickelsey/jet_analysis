67ec0f96ebd8f3e3563dfb8ab702ec5d42965f62
nickelsey <nicholas.elsey@gmail.com>
2018-01-14 19:29:01 -0500

    adding a function to convert pythia events to pseudojets

--- jet_analysis/util/vector_conversion.hh
+++ jet_analysis/util/vector_conversion.hh
@@ -9,11 +9,17 @@
 #include "TStarJetVectorContainer.h"
 #include "TStarJetVector.h"
 
+#include "Pythia8/Pythia.h"
+
 #include <random>
 
 // converts a TStarJetVectorContainer of TStarJetVectors
 // into an std::vector of fastjet::PseudoJets
 void ConvertTStarJetVector(TStarJetVectorContainer<TStarJetVector>* container, std::vector<fastjet::PseudoJet>& particles) {
+  
+  // resize container
+  size_t offset = particles.size();
+  particles.resize(particles.size() + container->GetEntries());
   
   // Transform TStarJetVectors into (FastJet) PseudoJets
   // ---------------------------------------------------
@@ -23,8 +29,25 @@ void ConvertTStarJetVector(TStarJetVectorContainer<TStarJetVector>* container, s
     
     fastjet::PseudoJet tmpPJ = fastjet::PseudoJet(*sv);
     tmpPJ.set_user_index(sv->GetCharge());
-    particles.push_back(tmpPJ);
+    particles[offset + i] = tmpPJ;
   }
+}
+
+void ConvertPythiaToPseudoJet(const Pythia8::Pythia& gen, std::vector<fastjet::PseudoJet>& particles) {
+  
+  size_t offset = particles.size();
+  size_t final_state = 0;
+  particles.resize(offset + gen.event.size());
+  
+  for (size_t i = 0; i < gen.event.size(); ++i) {
+    if ( gen.event[i].isFinal() && gen.event[i].isVisible() ) {
+      fastjet::PseudoJet tmp(gen.event[i].px(), gen.event[i].py(), gen.event[i].pz(), gen.event[i].e());
+      tmp.set_user_index(gen.event[i].charge());
+      particles[offset + final_state] = tmp;
+      ++final_state;
+    }
+  }
+  particles.resize(offset + final_state);
 }
 
 #endif // VECTOR_CONVERSION_HH
