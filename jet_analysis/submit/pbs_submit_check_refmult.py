b9ea238d054487e8000e69c9be8b1be350fa4967
nickelsey <nicholas.elsey@gmail.com>
2018-01-17 16:54:35 -0500

    had to remove some old code from refmult submit script

--- jet_analysis/submit/pbs_submit_check_refmult.py
+++ jet_analysis/submit/pbs_submit_check_refmult.py
@@ -73,20 +73,6 @@ def main(args) :
   ## get max number of jobs to be submitted to pbs
   ## at a time
   maxjobs = args.maxjobs
-  
-  ## read in embedding data file
-  ## create a list of embedding data if requested
-  embedding_list = []
-  if args.embed is not None :
-    with open(args.embed, 'r') as fp :
-      line = fp.readline()
-      while line :
-        embedding_list.append(line)
-        line = fp.readline()
-
-  ## if no valid embedding data was given, fill w/ empty
-  if not embedding_list:
-    embedding_list = ['']
 
   ## some paths
   execpath = os.getcwd()
