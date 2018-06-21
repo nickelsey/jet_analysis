701c705c517e8741d04dc82c3c555d4bb9e1a731
nickelsey <nicholas.elsey@gmail.com>
2018-06-20 22:12:50 -0400

    updating pt analysis to do all centralities at once

--- jet_analysis/data_qa/generate_pt_spectra.cc
+++ jet_analysis/data_qa/generate_pt_spectra.cc
@@ -125,11 +125,12 @@ int main(int argc, char* argv[]) {
   // initialize centrality definition for run 14
   // and set a lower limit on 0-20% for year 7
   CentralityRun14 centrality;
-  //int year7_centrality_cut = 269;
-  int year7_centrality_cut = 485;
-  // define upper and lower cut for 40-50%
-  //int year7_centrality_cut_upper = 114;
-  //int year7_centrality_cut_lower = 69;
+
+  int cent_bins = 8;
+  std::vector<std::pair<int, int>> CentBoundariesY7{{399, 1000}, {269, 398}, {178, 268}, {114, 177},
+                                          {69, 113}, {39, 68}, {21, 38}, {10, 20}};
+  std::vector<std::pair<int, int>> CentBoundariesY14{{0,1}, {2, 3}, {4, 5}, {6, 7}, {8, 9},
+                                            {10, 11}, {12, 13}, {14, 15}};
   
   // change to output file
   out.cd();
@@ -140,20 +141,24 @@ int main(int argc, char* argv[]) {
   TH3::SetDefaultSumw2();
   
   // create pT histogram
-  TH1D* pt = new TH1D("pt", ";p_{T}", 100, 0, 5);
-  TH1D* pt_corr = new TH1D("ptcorr", ";p_{T}", 100, 0, 5);
-  TH1D* refmult = new TH1D("refmult", ";refmult", 800, 0, 800);
-  TH1D* frac = new TH1D("discarded", "", 10, 0, 1.0);
-  TH1D* nprim = new TH1D("nprim", "", 100, 0, 2000);
-  TH1D* nsel = new TH1D("nsel", "", 100, 0, 2000);
-  TProfile* avg_eff = new TProfile("eff", "", 100, 0, 5.0);
-  TH1D* nhitsfit = new TH1D("nhitsfit", "", 50, 0, 50);
-  TH1D* nhitspos = new TH1D("nhitspos", "", 50, 0, 50);
-  TH1D* nhitsfitfrac = new TH1D("nhitsfitfrac", "", 50, 0, 1.0);
-  TH2D* dca = new TH2D("dcapt", "", 50, 0, 3, 50, 0, 5);
+  TH2D* pt = new TH2D("pt", ";p_{T};centrality", 100, 0, 5, cent_bins, -0.5, cent_bins - 0.5);
+  TH2D* pt_corr = new TH2D("ptcorr", ";p_{T};centrality", 100, 0, 5,cent_bins, -0.5, cent_bins - 0.5);
+  TH2D* refmult = new TH2D("refmult", ";refmult;centrality", 800, 0, 800, cent_bins, -0.5, cent_bins - 0.5);
+  TH2D* frac = new TH2D("discarded", "", 10, 0, 1.0, cent_bins, -0.5, cent_bins - 0.5);
+  TH2D* nprim = new TH2D("nprim", "", 100, 0, 2000, cent_bins, -0.5, cent_bins - 0.5);
+  TH2D* nsel = new TH2D("nsel", "", 100, 0, 2000, cent_bins, -0.5, cent_bins - 0.5);
+  TH2D* nhitsfit = new TH2D("nhitsfit", "", 50, 0, 50, cent_bins, -0.5, cent_bins - 0.5);
+  TH2D* nhitspos = new TH2D("nhitspos", "", 50, 0, 50, cent_bins, -0.5, cent_bins - 0.5);
+  TH2D* nhitsfitfrac = new TH2D("nhitsfitfrac", "", 50, 0, 1.0, cent_bins, -0.5, cent_bins - 0.5);
+  TH3D* dca = new TH3D("dcapt", "", 50, 0, 3, 50, 0, 5, cent_bins, -0.5, cent_bins - 0.5);
   
-  TH1D* nhitpos_outer_vz = new TH1D("outervz", "", 60, 0, 60);
-  TH1D* nhitpos_inner_vz = new TH1D("innervz", "", 60, 0, 60);
+  TH2D* nhitpos_outer_vz = new TH2D("outervz", "", 60, 0, 60, cent_bins, -0.5, cent_bins - 0.5);
+  TH2D* nhitpos_inner_vz = new TH2D("innervz", "", 60, 0, 60, cent_bins, -0.5, cent_bins - 0.5);
+  
+  std::vector<TProfile*> avg_eff(8);
+  for (int i = 0; i < 8; ++i) {
+    avg_eff[i] = new TProfile(MakeString("eff", i).c_str(), "", 100, 0, 5.0);
+  }
   
   // 2D eta/phi histograms
   TH2D* etaphi_pt0 = new TH2D("etaphi0", ";#eta;#phi", 40, -1, 1, 40, -TMath::Pi(), TMath::Pi());
@@ -173,7 +178,7 @@ int main(int argc, char* argv[]) {
     TStarJetPicoEventHeader* header = event->GetHeader();
     
     
-    int cent_bin = 0;
+    int cent_bin = -1;
     // check if event fired a trigger we will use
     if (triggers.size() != 0) {
       bool use_event = false;
@@ -184,18 +189,31 @@ int main(int argc, char* argv[]) {
     }
     
     if (opts.useY7Eff) {
-      if (header->GetGReferenceMultiplicity() < year7_centrality_cut)
-        continue;
-      refmult->Fill(header->GetGReferenceMultiplicity());
+      for (int i = 0; i < cent_bins; ++i) {
+        if (header->GetGReferenceMultiplicity() >= CentBoundariesY7[i].first &&
+            header->GetGReferenceMultiplicity() < CentBoundariesY7[i].second) {
+          cent_bin = i;
+          break;
+        }
+      }
+      refmult->Fill(header->GetGReferenceMultiplicity(), cent_bin);
     }
     
     if (opts.useY14Eff) {
       centrality.setEvent(header->GetRunId(), header->GetReferenceMultiplicity(),
                           header->GetZdcCoincidenceRate(), header->GetPrimaryVertexZ());
-      if (centrality.centrality16() > 0 || centrality.centrality16() < 0)
+      if (centrality.centrality16() < 0 || centrality.centrality16() > 15)
         continue;
-      refmult->Fill(header->GetReferenceMultiplicity());
-      cent_bin = centrality.centrality16();
+      int cent_bin_tmp = centrality.centrality16();
+      for (int i = 0; i < cent_bins; ++i) {
+        if (cent_bin_tmp <CentBoundariesY14[i].first || cent_bin_tmp > CentBoundariesY14[i].second)
+          continue;
+        cent_bin = i;
+        break;
+      }
+      if (cent_bin == -1)
+        continue;
+      refmult->Fill(header->GetReferenceMultiplicity(), cent_bin);
     }
     
     TStarJetVectorContainer<TStarJetVector>* container = reader->GetOutputContainer();
@@ -209,15 +227,15 @@ int main(int argc, char* argv[]) {
       if (fabs(track->GetEta()) > opts.eta || track->GetPt() < 0.2)
         continue;
       selected++;
-      nhitsfit->Fill(track->GetNOfFittedHits());
-      dca->Fill(track->GetDCA(), track->GetPt());
-      nhitspos->Fill(track->GetNOfPossHits());
-      nhitsfitfrac->Fill((double)track->GetNOfFittedHits()/track->GetNOfPossHits());
+      nhitsfit->Fill(track->GetNOfFittedHits(),cent_bin);
+      dca->Fill(track->GetDCA(), track->GetPt(), cent_bin);
+      nhitspos->Fill(track->GetNOfPossHits(), cent_bin);
+      nhitsfitfrac->Fill((double)track->GetNOfFittedHits()/track->GetNOfPossHits(), cent_bin);
       
       if (fabs(header->GetPrimaryVertexZ()) < 10)
-        nhitpos_inner_vz->Fill(track->GetNOfPossHits());
+        nhitpos_inner_vz->Fill(track->GetNOfPossHits(), cent_bin);
       if (fabs(header->GetPrimaryVertexZ()) > 25)
-        nhitpos_outer_vz->Fill(track->GetNOfPossHits());
+        nhitpos_outer_vz->Fill(track->GetNOfPossHits(), cent_bin);
       
       if (track->GetPt() > 0.2 && track->GetPt() < 0.5)
         etaphi_pt0->Fill(track->GetEta(), track->GetPhi());
@@ -228,48 +246,41 @@ int main(int argc, char* argv[]) {
       else {
         etaphi_pt3->Fill(track->GetEta(), track->GetPhi());
       }
-    }
-    nsel->Fill(selected);
-    
-    TStarJetVector* sv;
-    if (opts.useY7Eff) {
-      for (int i = 0; i < container->GetEntries(); ++i) {
-        sv = container->Get(i);
-        if (sv->GetCharge() && fabs(sv->Eta()) < opts.eta && sv->Pt() > 0.2) {
-          double eff = run7Eff->AuAuEff(sv->Pt(), sv->Eta(), 0);
-          norm++;
-          if (eff <= 0.0 || eff > 1.0) {
-            counts++;
-            continue;
-          }
-          pt->Fill(sv->Pt());
-          pt_corr->Fill(sv->Pt(), 1.0 / eff);
-          avg_eff->Fill(sv->Pt(), eff);
-          
+      
+      // do efficiency corrected pt spectrum
+      double eff = -1;
+      if (opts.useY7Eff) {
+        if (cent_bin <= 2) {
+          int cent_bin_tmp = 0;
+          if (header->GetGReferenceMultiplicity() >= 485)
+            cent_bin_tmp = 0;
+          else if (header->GetGReferenceMultiplicity() >= 399)
+            cent_bin_tmp = 1;
+          else if (header->GetGReferenceMultiplicity() >= 269)
+            cent_bin_tmp = 2;
+          eff = run7Eff->AuAuEff(track->GetPt(), track->GetEta(), cent_bin_tmp);
         }
       }
-      nprim->Fill(norm);
-      frac->Fill(counts/norm);
+      else if (opts.useY14Eff) {
+        eff = run14Eff->AuAuEff(track->GetPt(), track->GetEta(), centrality.centrality16(),
+                                header->GetZdcCoincidenceRate());
+      }
+      
+      norm++;
+      
+      pt->Fill(track->GetPt(), cent_bin);
+      
+      if (eff < 0 || eff > 1.0) {
+        counts++;
+        continue;
+      }
+      avg_eff[cent_bin]->Fill(track->GetPt(), eff);
+      pt_corr->Fill(track->GetPt(), cent_bin);
       
     }
-    else if (opts.useY14Eff) {
-      for (int i = 0; i < container->GetEntries(); ++i) {
-        sv = container->Get(i);
-        if (sv->GetCharge() && fabs(sv->Eta()) < opts.eta && sv->Pt() > 0.2) {
-          double eff = run14Eff->AuAuEff(sv->Pt(), sv->Eta(), cent_bin, header->GetZdcCoincidenceRate());
-          norm++;
-          if (eff <= 0.0 || eff > 1.0) {
-            counts++;
-            continue;
-          }
-          pt->Fill(sv->Pt());
-          pt_corr->Fill(sv->Pt(), 1.0 / eff);
-          avg_eff->Fill(sv->Pt(), eff);
-        }
-      }
-      nprim->Fill(norm);
-      frac->Fill(counts/norm);
-    }
+    nsel->Fill(selected, cent_bin);
+    nprim->Fill(selected, cent_bin);
+    frac->Fill(counts/norm, cent_bin);
   }
   
   out.Write();
