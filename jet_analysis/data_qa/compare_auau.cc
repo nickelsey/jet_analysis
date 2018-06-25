973b1c288b4408b9dd837cc85dd0bb4638798f69
nickelsey <nicholas.elsey@gmail.com>
2018-06-24 22:46:38 -0400

    testing

--- jet_analysis/data_qa/compare_auau.cc
+++ jet_analysis/data_qa/compare_auau.cc
@@ -123,10 +123,10 @@ int main(int argc, char* argv[]) {
   TH3D* lumiTracks_vz = new TH3D("lumitracksvz", ";zdc rate [kHz];nglobal;nprimary", 100, 0, 100, 400, 0, 4000, 400, 0, 1200);
   TH2D* recalcRefMult_vz = new TH2D("refmultvz", ";refmult;recalc refmult", 400, 0, 800, 400, 0, 800);
   
-  TH1D* pt = new TH1D("pt", ";pt", 100, 0, 10);
-  TH1D* nhit = new TH1D("nhit", ";nhit", 50, 0, 50);
-  TH1D* nhitpos = new TH1D("nhitpos", ";nhitpos", 50, 0, 50);
-  TH1D* fitfrac = new TH1D("fitfrac", ";fitfrac", 100, 0, 1.0);
+  TH2D* pt = new TH2D("pt", ";pt", 9, -0.5, 8.5, 100, 0, 10);
+  TH2D* nhit = new TH2D("nhit", ";nhit", 9, -0.5, 8.5, 50, 0, 50);
+  TH2D* nhitpos = new TH2D("nhitpos", ";nhitpos", 9, -0.5, 8.5, 50, 0, 50);
+  TH2D* fitfrac = new TH2D("fitfrac", ";fitfrac", 9, -0.5, 8.5, 100, 0, 1.0);
   
   TH2D* etaphi = new TH2D("etaphi", ";eta;phi", 40, -1, 1, 40, -TMath::Pi(), TMath::Pi());
   
@@ -174,10 +174,10 @@ int main(int argc, char* argv[]) {
       if (fabs(track->GetEta()) < 1.0 && track->GetDCA() < 3.0 && track->GetNOfFittedHits() >= 10)
         nprimary++;
       
-      pt->Fill(track->GetPt());
-      nhit->Fill(track->GetNOfFittedHits());
-      nhitpos->Fill(track->GetNOfPossHits());
-      fitfrac->Fill((double)track->GetNOfFittedHits() / track->GetNOfPossHits());
+      pt->Fill(cent_bin, track->GetPt());
+      nhit->Fill(cent_bin, track->GetNOfFittedHits());
+      nhitpos->Fill(cent_bin, track->GetNOfPossHits());
+      fitfrac->Fill(cent_bin, (double)track->GetNOfFittedHits() / track->GetNOfPossHits());
       etaphi->Fill(track->GetEta(), track->GetPhi());
       
     }
