7ab9225d0a34b5797e4399c50294386d7d015597
nickelsey <nicholas.elsey@gmail.com>
2018-08-02 11:22:45 -0400

    adding gtest and benchmark to refactor testing

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -32,3 +32,27 @@ find_package(TStarJetPico REQUIRED)
 ## setup glog and gflags
 find_package(gflags REQUIRED)
 find_package(glog REQUIRED)
+
+#######################################
+## testing resources
+## googletest
+set(TEMP_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
+set(BUILD_SHARED_LIBS OFF)
+set(BUILD_GTEST ON)
+set(INSTALL_GTEST OFF)
+## gmock currently not used
+set(BUILD_GMOCK OFF)
+add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/googletest)
+include_directories(${PROJECT_SOURCE_DIR}/third_party/googletest/googletest/include)
+
+## benchmark
+# We will not need to test benchmark lib itself.
+set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "Disable benchmark testing.")
+set(BENCHMARK_ENABLE_INSTALL OFF CACHE BOOL "Disable benchmark install to avoid overwriting.")
+add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/benchmark)
+include_directories(${PROJECT_SOURCE_DIR}/third_party/benchmark/include)
+list(APPEND SFD_DEPENDENCY_LIBS benchmark)
+
+# restore the build shared libs option.
+set(BUILD_SHARED_LIBS ${TEMP_BUILD_SHARED_LIBS})
+
