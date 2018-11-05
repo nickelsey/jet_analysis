0dc577bddfd963f10de7c14869844ad71c8e6daf
nickelsey <nicholas.elsey@gmail.com>
2018-11-04 22:30:50 -0500

    updates for y11

--- jet_analysis/efficiency/run11_eff.cc
+++ jet_analysis/efficiency/run11_eff.cc
@@ -1,5 +1,5 @@
 #include "jet_analysis/efficiency/run11_eff.hh"
-
+#include <iostream>
 Run11Eff::Run11Eff(std::string file) {
 	LoadFile(file);
 }
@@ -7,14 +7,13 @@ Run11Eff::Run11Eff(std::string file) {
 bool Run11Eff::LoadFile(std::string file) {
 	input_ = new TFile(file.c_str(), "READ");
 	for (int i = 0; i < 9; ++i) {
-		std::string curvename = "" + std::to_string(i);
+		std::string curvename = "cent" + std::to_string(i);
 		curves_.push_back((TH2D*) input_->Get(curvename.c_str()));
 	}
 	return true;
 }
 
 double Run11Eff::AuAuEff(double pt, double eta, int cent) {
-
 	if (cent < 0 || cent > 8)
 		return 1.0;
 
@@ -26,6 +25,10 @@ double Run11Eff::AuAuEff(double pt, double eta, int cent) {
 	
 	if (fabs(eta) > 1.0)
 		return 1.0;
-
-	return curves_.at(cent)->GetBinContent(curves_.at(cent)->FindBin(pt, eta));
+	std::cout << "finding bins" << std::endl;
+	int binx = curves_.at(cent)->GetXaxis()->FindBin(pt);
+	int biny = curves_.at(cent)->GetYaxis()->FindBin(eta);
+	std::cout << "finding content" << std::endl;
+	double efficiency = curves_.at(cent)->GetBinContent(binx, biny);
+	return efficiency;
 }
